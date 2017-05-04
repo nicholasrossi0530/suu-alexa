14c245c85754b5e29946afca5c79a9615bb4f447
nrossi0530 <nrossi0530@gmail.com>
2017-05-04 13:22:44 -0700

    Final working submission

--- index.js
+++ index.js
@@ -2,19 +2,28 @@
 var Alexa = require('alexa-sdk');
 
 var APP_ID = undefined; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
-var SKILL_NAME = 'On Campus Dining';
+var SKILL_NAME = 'Thunderbird Dining';
 
 var dietaryRestriction = "";
-// TODO: probably set default to None
 var meal = "";
 
 function greeting() {
-    return "Welcome to Thunderbird Dining!";
+    return "Welcome to Thunderbird Dining! What meal will you be eating today?";
 }
 
 function scrapeDining(result) {
-    // TODO: grab todays date
-    var url = 'https://new.dineoncampus.com/v1/location/menu.json?date=2017-4-20&location_id=587e5ede3191a20db79dd2bc&platform=0&site_id=5751fd3790975b60e04893f0'
+    var url = '';
+    var today = new Date();
+    var d = today.getDate();
+    var m = today.getMonth()+1;
+    var yyyy = today.getFullYear();
+    // Check for summer break
+    // if(m < 5 || (m > 7 && d > 13)){
+        url = 'https://new.dineoncampus.com/v1/location/menu.json?date='+yyyy+'-4-9&location_id=587e5ede3191a20db79dd2bc&platform=0&site_id=5751fd3790975b60e04893f0'
+    // }
+    // else {
+    //     url = 'https://new.dineoncampus.com/v1/location/menu.json?date='+yyyy+'-'+m+'-'+d+'&location_id=587e5ede3191a20db79dd2bc&platform=0&site_id=5751fd3790975b60e04893f0';
+    // }
     var http = require('https');
     http.get(url, function(res){
         var body = '';
@@ -37,71 +46,6 @@ function chooseMeal(meals) {
         meals(result.menu.periods);
     });
 }
-
-function setDietaryRestriction() {
-    dietaryRestriction = "Vegetarian";
-}
-
-function listMeals() {
-    chooseMeal(function(meals){
-        if(dietaryRestriction == ""){
-            console.log("empty");
-        }     
-        else{
-            console.log(dietaryRestriction);
-        }
-            var result = "";
-            for(var m in meals){
-                result += meals[m].name + ", ";
-            }
-            var speechOutput = "The meals are " + result +  ".";
-            console.log(speechOutput);
-        });
-}
-
-function getMeals() {
-    chooseMeal(function(meals){
-            var chosenMeal = meals;
-            var mealList = "Your options for ";
-            switch(meal){
-                case "Breakfast":
-                    chosenMeal = chosenMeal[0].categories;
-                    mealList += "breakfast are: ";
-                    break;
-                case "Lunch":
-                    chosenMeal = chosenMeal[1].categories;
-                    mealList += "lunch are: ";
-                    break;
-                case "Dinner":
-                    chosenMeal = chosenMeal[2].categories;
-                    mealList += "dinner are: ";
-                    break;
-                case "Brunch":
-                    chosenMeal = chosenMeal[3].categories;
-                    mealList += "brunch are: ";
-                    break;
-            }
-            for(var category in chosenMeal){
-                for(var items in chosenMeal[category].items){
-                    //console.log(chosenMeal[category].items[items].name);
-                    if(dietaryRestriction == "None"){
-                        mealList += chosenMeal[category].items[items].name + "\n";
-                    }
-                    else {
-                        for(var filters in items){
-                            if(chosenMeal[category].items[items].filters[filters].name == dietaryRestriction){
-                                mealList += chosenMeal[category].items[items].name + ", ";
-                            }
-                        }
-                    }
-                }
-            }
-            console.log(mealList);
-        });
-}
-
-getMeals();
-
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
@@ -128,12 +72,10 @@ var handlers = {
     'ChooseVegetarianIntent': function () {
         dietaryRestriction = "Vegetarian";
         this.emit('CheckMealAndDiet');
-        // this.emit(':tell', dietaryRestriction + " " + meal);
     },
     'ChooseVeganIntent': function () {
         dietaryRestriction = "Vegan";
         this.emit('CheckMealAndDiet');
-        // this.emit(':tell', dietaryRestriction + " " + meal);
     },
     'ChooseNoneIntent': function () {
         dietaryRestriction = "None";
@@ -142,33 +84,28 @@ var handlers = {
     'ChooseBreakfastIntent': function () {
         meal = "Breakfast";
         this.emit('CheckMealAndDiet');
-        // this.emit(':tell', dietaryRestriction + " " + meal);
     },
     'ChooseLunchIntent': function () {
         meal = "Lunch";
         this.emit('CheckMealAndDiet');
-        // this.emit(':tell', dietaryRestriction + " " + meal);
     },
     'ChooseDinnerIntent': function () {
         meal = "Dinner";
         this.emit('CheckMealAndDiet');
-        // this.emit(':tell', dietaryRestriction + " " + meal);
     },
     'ChooseBrunchIntent': function () {
         meal = "Brunch";
         this.emit('CheckMealAndDiet');
-        // this.emit(':tell', dietaryRestriction + " " + meal);
     },
     'CheckMealAndDiet': function () {
-        // TODO: rearrange when default is None
-        if(meal !== "" && dietaryRestriction == "") {
-            this.emit(':tell', "Do you have a dietary restriction or would you like to hear the meals?");
+        if(meal && !dietaryRestriction) {
+            this.emit(':tell', "Do you have a dietary restriction?");
         }
-        else if(meal == "" && dietaryRestriction !== "") {
+        else if(!meal && dietaryRestriction) {
             this.emit(':tell', "What meal will you be eating?");
         }
-        else if(meal == "" && dietaryRestriction == ""){
-            this.emit(':tell', "Let me know what meal you would like to eat as well as any dietary restrictions you may have.");
+        else if(!meal && !dietaryRestriction){
+            this.emit(':tell', "Let me know which meal you would like to eat as well as any dietary restrictions you may have.");
         }
         else {
             this.emit('PullMeals');
@@ -199,9 +136,8 @@ var handlers = {
             }
             for(var category in chosenMeal){
                 for(var items in chosenMeal[category].items){
-                    //console.log(chosenMeal[category].items[items].name);
-                    if(dietaryRestriction == "None"){
-                        mealList += chosenMeal[category].items[items].name + "\n";
+                    if(dietaryRestriction == "None" && !(/['"/&]/.test(chosenMeal[category].items[items].name))){
+                        mealList += chosenMeal[category].items[items].name + ", ";
                     }
                     else {
                         for(var filters in items){
@@ -221,9 +157,9 @@ var handlers = {
         this.emit(':ask', speechOutput, reprompt);
     },
     'AMAZON.CancelIntent': function () {
-        this.emit(':tell', 'Goodbye!');
+        this.emit(':tell', 'Enjoy your meal!');
     },
     'AMAZON.StopIntent': function () {
-        this.emit(':tell', 'Goodbye!');
+        this.emit(':tell', 'Enjoy your meal!');
     }
 };
